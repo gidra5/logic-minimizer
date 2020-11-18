dadcdf0cdbe385663a5e8db9179305ac0c8502ec
gidra5 <hranitel.52@gmail.com>
2020-11-19 01:52:57 +0200

    almost working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::{env, fs};
-mod intersect;
-use intersect::intersect;
+mod simplified;
+use simplified::simplify;
 
 #[derive(Debug, Clone)]
-struct Implicant {
-    terms: Vec<Option<bool>>
+pub struct Implicant {
+    terms: Vec<Option<bool>>,
 }
 
 impl PartialEq for Implicant {
@@ -15,7 +15,7 @@ impl PartialEq for Implicant {
 
 #[derive(Debug)]
 struct LogicalFunction {
-    implicants: Vec<Implicant>
+    implicants: Vec<Implicant>,
 }
 
 fn main() {
@@ -23,72 +23,91 @@ fn main() {
         let args: Vec<String> = env::args().collect();
         let file = &args[1];
 
-        fs::read_to_string(&file).unwrap()
+        fs::read_to_string(&file)
+            .unwrap()
             .split("\r\n")
             .filter(|x| *x != "")
             .map(|x| {
                 let collected: Vec<&str> = x.split('~').collect();
-                let c1: Vec<Option<bool>> = collected[0].split("")
+
+                let c1: Vec<Option<bool>> = collected[0]
+                    .split("")
                     .filter(|x| *x != "")
                     .map(|x| match x {
                         "0" => Some(false),
                         "1" => Some(true),
-                        _   => None
-                    }).collect();
-                let c2: Vec<String> = collected[1].split("")
+                        _ => None,
+                    })
+                    .collect();
+
+                let c2: Vec<String> = collected[1]
+                    .split("")
                     .filter(|x| *x != "")
                     .map(|x| String::from(x))
                     .collect();
 
                 (Implicant { terms: c1 }, c2)
-            }).collect()
+            })
+            .collect()
     };
 
     println!("Initial:");
     for i in &implicants {
         println!("{:?}", i);
-    }    
+    }
+
     println!("");
 
-    let mut simplified: Vec<(Implicant, Vec<String>)> = vec![];
+    let simplified: Vec<(Implicant, Vec<String>)> = simplify(implicants);
 
-    for i in 0..implicants.len() {
-        let itemI = &implicants[i];
-        let mut found = false;
-        for j in i + 1..implicants.len() {
-            let itemJ = &implicants[j];
-            let compared: Vec<bool> = itemI.1.iter().zip(itemJ.1.iter())
-                .map(|(a, b)| a == "1" && b == "1")
-                .collect();
-            if (compared.contains(&true)) {
-                let pairs = itemI.0.terms.iter().zip(itemJ.0.terms.iter());
-                let mut counter = vec![];
-                for (i, (a, b)) in pairs.enumerate() {
-                    if (a != b) { counter.push(i); }         
-                };
-                if (counter.len() == 1) { 
-                    let mut simpler = itemI.0.terms.clone();
-                    let index = counter[0];
-                    simpler[index] = None;
-                    let implicant = Implicant { terms: simpler };
-                    let intersect = compared.iter()
-                        .map(|x| match x {
-                            true =>  String::from("1"),
-                            false => String::from("0")
-                        })
-                        .collect();
-                    found = true;
-                    simplified.push((implicant, intersect ));
-                };
-            };
-        };
+    // for i in 0..implicants.len() {
+    //     let item_i = &implicants[i];
+    //     let mut found = false;
 
-        if !found { simplified.push(itemI.clone()); };
-    };
+    //     for j in i + 1..implicants.len() {
+    //         let item_j = &implicants[j];
+    //         let compared: Vec<bool> = item_i
+    //             .1
+    //             .iter()
+    //             .zip(item_j.1.iter())
+    //             .map(|(a, b)| a == "1" && b == "1")
+    //             .collect();
+
+    //         if compared.contains(&true) {
+    //             let pairs = item_i.0.terms.iter().zip(item_j.0.terms.iter());
+    //             let mut counter = vec![];
+
+    //             for (i, (a, b)) in pairs.enumerate() {
+    //                 if a != b {
+    //                     counter.push(i);
+    //                 }
+    //             }
+
+    //             if counter.len() == 1 {
+    //                 let mut simpler = item_i.0.terms.clone();
+    //                 let index = counter[0];
+    //                 simpler[index] = None;
+    //                 let implicant = Implicant { terms: simpler };
+    //                 let intersect = compared
+    //                     .iter()
+    //                     .map(|x| match x {
+    //                         true => String::from("1"),
+    //                         false => String::from("0"),
+    //                     })
+    //                     .collect();
+    //                 found = true;
+    //                 simplified.push((implicant, intersect));
+    //             };
+    //         };
+    //     }
+
+    //     if !found {
+    //         simplified.push(item_i.clone());
+    //     };
+    // }
 
     println!("Simplified:");
     for i in &simplified {
         println!("{:?}", i);
     }
 }
-
--- src/simplified.rs
+++ src/simplified.rs
@@ -1,46 +1,64 @@
-pub fn simplify(implicants: Vec<(Implicant, Vec<String>)>) -> Vec<(Implicant, Vec<String>)> {
-  let mut simplified: Vec<(Implicant, Vec<String>)> = vec![];
+pub use crate::Implicant;
+
+pub fn simplify(implicants: Vec<( Implicant, Vec<String> )>) -> Vec<( Implicant, Vec<String> )> {
+  let mut simplified_implicants: Vec<Vec<String>> = 
+    implicants.iter().map(|(_, vec)| vec.clone()).collect();
+  let mut simplified: Vec<( Implicant, Vec<String> )> = vec![];
 
   for i in 0..implicants.len() {
-    let itemI = &implicants[i];
-    let mut found = false;
-  
+    let item_i = &implicants[i];
+
     for j in i + 1..implicants.len() {
-      let itemJ = &implicants[j];
-      let compared: Vec<bool> = itemI.1.iter().zip(itemJ.1.iter())
+      let item_j = &implicants[j];
+
+      let intersect: Vec<String> = item_i.1.iter()
+        .zip(item_j.1.iter())
         .map(|(a, b)| a == "1" && b == "1")
+        .map(|x| match x {
+          true =>  String::from("1"),
+          false => String::from("0")
+        })
         .collect();
-      if (compared.contains(&true)) {
-        let pairs = itemI.0.terms.iter().zip(itemJ.0.terms.iter());
-        let mut counter = vec![];
-        for (i, (a, b)) in pairs.enumerate() {
-          if (a != b) { counter.push(i); }         
-        };
-        if (counter.len() == 1) { 
-          let mut simpler = itemI.0.terms.clone();
-          let index = counter[0];
-          simpler[index] = None;
+
+      if intersect.contains(&String::from("1")) {
+
+        let different_at: Vec<usize> = item_i.0.terms.iter()
+          .zip(item_j.0.terms.iter())
+          .enumerate()
+          .filter(|(_, (a, b))| a != b )
+          .map(|(i, _)| i)
+          .collect();
+
+        if different_at.len() == 1 { 
+          println!("{}, {}, {:?}", i, j, intersect);
+          let mut simpler = item_i.0.terms.clone();
+          simpler[different_at[0]] = None;
   
           let implicant = Implicant { terms: simpler };
-          let intersect = compared.iter()
-            .map(|x| match x {
-              true =>  String::from("1"),
-              false => String::from("0")
-            })
-            .collect();
-          found = true;
-          simplified.push((implicant, intersect ));
+          
+          //TODO set to "0" elements that are "1" in 'intersect'
+          // simplified_implicants[i] = true;
+          // simplified_implicants[j] = true;
+
+          simplified.push(( implicant, intersect ));
         };
       };
     };
-  
-    if !found { simplified.push(itemI.clone()); };
   };
 
-  match simplified {
-    1 => simplified
-    2 => simplify(simplified)
-  }
+  //for now to debug
+  // if simplified.len() > 0 {
+  //   simplified = simplify(simplified);
+  // }
+
+  let mut tmp: Vec<( Implicant, Vec<String> )> = implicants.into_iter()
+    .enumerate()
+    .filter(|(i, _)| simplified_implicants[*i].iter().fold(true, |acc, x| acc || x == "1"))
+    .map(|(_, x)| x)
+    .collect();
+
+  tmp.append(&mut simplified);
+  tmp
 }
 
 
