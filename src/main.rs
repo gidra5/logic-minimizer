836091826e25ff206effa2c2f1d149908497c83f
AliusDieMorietur <ikamqwerty@gmail.com>
2020-11-24 22:17:40 +0200

    till the end

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, fs};
 use std::thread;
+use std::sync::{Arc, Mutex};
 use std::fmt::Display;
 use itertools::Itertools;
 
@@ -178,8 +179,9 @@ fn main() {
     // }
     // println!("");
 
-    let threads_quantity = implicants[0].1.len();
+    let function_variants = Arc::new(Mutex::new(vec![]));
 
+    let threads_quantity = implicants[0].1.len();
     let mut threads = vec![];
     for i in 0..threads_quantity {
         let one_fn: Vec<_> = implicants.clone().iter()
@@ -187,34 +189,35 @@ fn main() {
             .collect();
             
         threads.push(
-            thread::spawn(move || {
+            thread::spawn({let clone = Arc::clone(&function_variants); move || {
                 let simplified: Vec<_> = simplify(&one_fn);
                 let (core, others) = construct_func(&one_fn, simplified);
 
-                let mut variants = vec![];
-                let mut it = others.into_iter()
+                let mut impicant_variants = vec![];
+                let mut implicant_iter = others.into_iter()
                     .map(|x| x.into_iter())
                     .multi_cartesian_product()
                     .peekable();
                 
-                match it.peek() {
+                match implicant_iter.peek() {
                     Some(_) => (),
-                    None => variants.push( LogicalFunction { implicants: core.clone() })
+                    None => impicant_variants.push( LogicalFunction { implicants: core.clone() })
                 }
 
-                for combination in it {
+                for combination in implicant_iter {
                     let mut implicants = core.clone();
                     implicants.append(&mut combination.into_iter()
                         .unique()
                         .collect::<Vec<_>>());
                     
-                    variants.push(LogicalFunction { implicants });
+                    impicant_variants.push(LogicalFunction { implicants });
                 }
 
-                let count = variants[0].implicants.len();
+
+                let count = impicant_variants[0].implicants.len();
                 let mut shortest_variants = vec![];
 
-                for variant in variants {
+                for variant in impicant_variants {
                     if variant.implicants.len() < count {
                         shortest_variants = vec![variant];
                     } else if variant.implicants.len() == count {
@@ -229,12 +232,74 @@ fn main() {
                     .collect::<Vec<_>>()
                     .join(" or\n")
                 );
-            })
+
+                let mut v = clone.lock().unwrap();
+                v.push((i, shortest_variants));
+            }})
         );
     }
 
     for i in threads {
         i.join().unwrap();
     }
-    
+
+    let bind = function_variants.lock().unwrap();
+    let with_numbers = bind.iter()
+      .map(|(i, x)| (i, x.iter()
+        .map(|y| y.implicants.iter()
+            .map(|z| z.clone().terms).collect::<Vec<_>>()
+        ).collect::<Vec<_>>())
+      );
+    let without_numbers: Vec<_> = with_numbers.clone()
+      .map(|(_i, x)| x).collect(); 
+
+    println!("\nWITH NUMBERS");
+    for i in with_numbers.clone() {
+        println!("{:?}", i);
+    }
+
+    println!("\nWITHOUT NUMBERS");
+    for i in without_numbers.clone() {
+        println!("{:?}", i);
+    }
+
+    let order: Vec<_> = with_numbers.clone().map(|(i, x)| i).collect();
+
+    let mut implicant_iter = with_numbers.clone()
+        .map(|(_, x)| x.into_iter())
+        .multi_cartesian_product()
+        .peekable();
+
+    let combined: Vec<_> = implicant_iter.clone().collect();
+
+    let mut res = combined[0].clone();
+    let mut global_counter = 0;
+
+    for (i_j, j) in res.iter().clone().enumerate() {
+        for (i_k, k) in  res.iter().clone().enumerate() {
+            if i_j == i_k { continue; };
+            if j == k { global_counter = global_counter + 1; };
+        }
+    }
+
+    for (index, i) in implicant_iter.clone().enumerate() {
+        let mut counter = 0;
+        let mapped = i.iter()
+          .map(|x| x[0].clone());
+        for (i_j, j) in mapped.clone().enumerate() {
+            for (i_k, k) in mapped.clone().enumerate() {
+                if i_j == i_k { continue; };
+                if j == k { counter = counter + 1; };
+            }
+        }
+        if counter > global_counter {
+            global_counter = counter;
+            res = i;
+        };
+    }
+
+    println!("\nRES:");
+    for i in order {
+        println!("y{}={:?}", i, res[*i]);
+    }
 }
